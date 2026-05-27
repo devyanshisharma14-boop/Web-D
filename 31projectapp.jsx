df76138b46ba92bcf9f4645fa44061deb9be0553
Devyanshi Sharma <devyanshi.25b01010298@abes.ac.in>
2026-05-27 12:47:44 +0530

    updated project

---
+++ 31projectapp.jsx
@@ -0,0 +1,38 @@
+body {
+  background-color: #f4f4f4;
+  font-family: Arial, sans-serif;
+}
+
+.container {
+  width: 260px;
+  margin: 10px auto;
+  text-align: center;
+  border: 2px solid black;
+  padding: 10px;
+}
+
+.container h1 {
+  font-size: 28px;
+}
+
+.card {
+  background: white;
+  padding: 10px;
+  margin: 10px 0;
+  border-radius: 8px;
+  box-shadow: 0 0 5px gray;
+}
+
+.card h2 {
+  font-size: 18px;
+}
+
+.card p {
+  font-size: 14px;
+}
+
+.details {
+  margin-top: 10px;
+  font-size: 14px;
+  font-weight: bold;
+}
