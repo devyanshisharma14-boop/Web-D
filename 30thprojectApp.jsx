9370f207bfcce09e572f979a4389bb49a3802c41
Devyanshi Sharma <devyanshi.25b01010298@abes.ac.in>
2026-05-27 12:45:30 +0530

    updated project

---
+++ 30thprojectApp.jsx
@@ -0,0 +1,45 @@
+import './App.css';
+
+function Student(props) {
+  return (
+    <div className="card">
+      <h2>{props.name}</h2>
+      <p>Course: {props.course}</p>
+      <p>Marks: {props.marks}</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="container">
+
+      <h1>Student Information</h1>
+
+      <Student
+        name="Rahul Sharma"
+        course="Computer Science"
+        marks="85"
+      />
+
+      <Student
+        name="Anita Verma"
+        course="Information Technology"
+        marks="92"
+      />
+
+      <Student
+        name="Rohan Gupta"
+        course="Electronics"
+        marks="78"
+      />
+
+      <p className="details">
+        Name: Tameesh Gupta <br />
+        Section: CSE-26 <br />
+        Roll No: 2503201001217
+      </p>
+
+    </div>
+  );
+}
